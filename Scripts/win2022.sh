f3283086038c5a4acdc0ed73199c35f4ce6e8860
Tannerru <tanner.rusher@outlook.com>
2024-01-04 21:41:16 -0600

    test #7

--- Scripts/win2022.sh
+++ Scripts/win2022.sh
@@ -1,6 +1,4 @@
 #!/bin/sh
-connect-host: $OP_CONNECT_HOST
-connect-token: ${{ secrets.OP_CONNECT_TOKEN }}
 eval $(op signin)
 cd ./Windows/Server_2022/
 op inject -i build.pkrvars.hcl -o build.auto.pkrvars.hcl
